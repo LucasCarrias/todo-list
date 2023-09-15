d42df8964018d56fd58769334aacbbe33c5c2601
Lucas Carrias <lucascarrias@outlook.com>
2023-09-15 12:24:55 -0300

    Refactor plus sign

--- src/components/NewTaskBar.tsx
+++ src/components/NewTaskBar.tsx
@@ -1,14 +1,14 @@
-import styles from "./NewTaskBar.module.css";
+import { PlusCircle } from "@phosphor-icons/react";
 
-import plusIcon from "../assets/plus.svg";
+import styles from "./NewTaskBar.module.css";
 
 export function NewTaskBar() {
   return (
     <div className={styles.container}>
       <input className={styles.input} placeholder="Adicione uma nova tarefa" />
       <button className={styles.button}>
-        Criar
-        <img src={plusIcon} />
+        <span>Criar</span>
+        <PlusCircle  size={24}/>
       </button>
     </div>
   );
